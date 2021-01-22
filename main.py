8e43953df22a505cde30afd8ec4b64adcb76f93a
ericaf7923 <72775447+ericaf7923@users.noreply.github.com>
2021-01-21 23:45:33 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+while True:
+    print("sound level :" +input.sound_level())
+    if input.sound_level()> 
+       music.play_melody("G E G GE G GE AGG E G E G GE G GE AGG E", 60)
+    music.set_volume(100)
