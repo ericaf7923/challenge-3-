125a00e3191c3b1966f2734cec319c28ea0291a0
ericaf7923 <72775447+ericaf7923@users.noreply.github.com>
2021-01-21 23:47:49 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-while True:
-    print("sound level :" +input.sound_level())
-    if input.sound_level()> 
-       music.play_melody("G E G GE G GE AGG E G E G GE G GE AGG E", 60)
-    music.set_volume(100)
+#while True
+    #print("sound level :" +input.sound_level())
+    #if input.sound_level()>5
+    #lights.set_all(lights.rgb(255, 0, 255))
+    #else: 
+        #light.set_all(light.rgb(255, 0, 255))
+while True: 
+    print("light level: " + str(input.light_level()))
+    if input.light_level() >11: 
+        music.power_down.play_until_done()
+    else: 
+        music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+// while True
+// print("sound level :" +input.sound_level())
+// if input.sound_level()>5
+// lights.set_all(lights.rgb(255, 0, 255))
+// else: 
+// light.set_all(light.rgb(255, 0, 255))
+while (true) {
+    console.log("light level: " + ("" + input.lightLevel()))
+    if (input.lightLevel() > 11) {
+        music.powerDown.playUntilDone()
+    } else {
+        music.stopAllSounds()
+    }
+    
+}
